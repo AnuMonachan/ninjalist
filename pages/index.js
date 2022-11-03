44ee4776736f412071cc57c582990e2c29e286a2
AnuMonachan <anumonachan9953@gmail.com>
2022-11-03 15:53:54 +0530

    updated title of imported package

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../comps/Navbar';
 import Footer from '../comps/Footer';
-import Link from 'next/Link'
+import Link from 'next/link'
 
 export default function Home() {
   return (
