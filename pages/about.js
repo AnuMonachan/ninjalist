ad11c3ffc02a73973c1b8cc8c159c0d4addd929f
AnuMonachan <anumonachan9953@gmail.com>
2022-11-03 15:37:03 +0530

    added ninjalist project

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+
+      <div>
+        <h1>About</h1>
+        <p>
+          There is no one who loves pain itself, who seeks after it and wants to
+          have it, simply because it is pain..
+        </p>
+        <p>
+          There is no one who loves pain itself, who seeks after it and wants to
+          have it, simply because it is pain..
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
